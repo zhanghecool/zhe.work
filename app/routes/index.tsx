897f863b47903438924a0a61ed20d93b4c32adbd
Zhang He <zhanghe@zhe.cool>
2022-02-13 10:18:49 +0800

    feat(posts): add posts

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,10 @@
+import { Link } from 'remix';
+
 export default function Index() {
   return (
     <div className='text-3xl font-bold underline'>
       <h1>你好，欢迎来到 ZHE.WORK</h1>
+      <Link to='/posts'>Posts</Link>
     </div>
   );
 }
